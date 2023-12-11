70dbcff7032d40aa3a90db1da4ea122140c23000
Kyle Horton <kylehorton33@gmail.com>
2023-12-11 15:28:05 -0500

    refactor: :card_file_box: refactor database tol `$lib/db`

--- client/src/routes/recipes/+page.server.js
+++ client/src/routes/recipes/+page.server.js
@@ -1,42 +1,13 @@
-export const load = () => {
-  const allRecipes = [
-    {
-      name: 'negroni',
-      color: '#820202',
-      ingredients: [
-        { name: 'gin', inStock: true, unit: 'oz' },
-        { name: 'Campari', inStock: true, unit: 'oz' },
-        { name: 'sweet vermouth', inStock: true, unit: 'oz' }
-      ]
-    },
-    {
-      name: 'daiquiri',
-      color: '#5fad47',
-      ingredients: [
-        { name: 'rum', inStock: false, unit: 'oz' },
-        { name: 'lime juice', inStock: false, unit: 'oz' },
-        { name: 'simple syrup', inStock: true, unit: 'oz' }
-      ]
-    },
-    {
-      name: 'Last Word',
-      color: '#7fb820',
-      ingredients: [
-        { name: 'gin', inStock: true, unit: 'oz' },
-        { name: 'green Chartreuse', inStock: true, unit: 'oz' },
-        { name: 'maraschino liqueur', inStock: false, unit: 'oz' },
-        { name: 'lime juice', inStock: false, unit: 'oz' }
-      ]
-    }
-  ]
+import { recipes } from '$lib/db'
 
-  allRecipes.forEach((r) => {
+export const load = () => {
+  recipes.forEach((r) => {
     r.missing = r.ingredients.reduce((n, i) => n + +!i.inStock, 0)
   })
 
-  allRecipes.sort((a, b) => a.missing - b.missing)
+  recipes.sort((a, b) => a.missing - b.missing)
 
   return {
-    recipes: allRecipes
+    recipes: recipes
   }
 }
