562d8e5eace71934f3267920ba232911cbcbf170
Kyle Horton <kylehorton33@gmail.com>
2023-12-11 16:21:02 -0500

    fill in some more blank content

--- client/src/routes/about/+page.svelte
+++ client/src/routes/about/+page.svelte
@@ -1,1 +1,11 @@
-TODO about
+<div class="flex flex-col items-center mt-4 p-4 space-y-4">
+  <h1 class="text-xl">About</h1>
+  <p>
+    mise is designed to keep track of your favorite recipes and available
+    ingredients in a clean, easy to use interface
+  </p>
+  <p>Designed by `kylehorton33` November 2023</p>
+  <a href="https://github.com/kylehorton33/recipe-pocketbase-svelte/"
+    >github.com/kylehorton33/recipe-pocketbase-svelte/</a
+  >
+</div>
--- client/src/routes/auth/my-account/+page.svelte
+++ client/src/routes/auth/my-account/+page.svelte
@@ -2,4 +2,7 @@
   import { user } from '$lib/stores'
 </script>
 
-<code>{$user}</code>
+<div class="flex flex-col items-center pt-8 space-y-4">
+  <h1>Account Settings</h1>
+  <code>{$user}</code>
+</div>
